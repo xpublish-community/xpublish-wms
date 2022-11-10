94d2ca7411345110104d7cab1b136899f14c1a21
Matt Iannucci <matt.iannucci@rpsgroup.com>
2022-11-10 10:46:32 -0500

    More project scaffolding

--- tests/test_cf_wms_router.py
+++ tests/test_cf_wms_router.py
@@ -2,6 +2,28 @@ import pytest
 import xpublish
 from fastapi.testclient import TestClient
 
-from xpublish_edr.cf_wms_router import cf_wms_router
+from xpublish_wms.cf_wms_router import cf_wms_router
 
-# TODO: Build out tests
+
+@pytest.fixture(scope="session")
+def cf_dataset():
+    from cf_xarray.datasets import airds
+
+    return airds
+
+
+@pytest.fixture(scope="session")
+def cf_xpublish(cf_dataset):
+    rest = xpublish.Rest({"air": cf_dataset}, routers=[cf_wms_router])
+
+    return rest
+
+
+@pytest.fixture(scope="session")
+def cf_client(cf_xpublish):
+    app = cf_xpublish.app
+    client = TestClient(app)
+
+    return client
+
+# TODO
