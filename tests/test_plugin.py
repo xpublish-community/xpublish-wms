a6bc184ccd821e7c1bb21b3d8c01233f93385f6c
Matt Iannucci <matt.iannucci@rpsgroup.com>
2023-07-11 14:48:12 -0400

    Update tests

--- tests/test_plugin.py
+++ tests/test_plugin.py
@@ -1,6 +1,7 @@
 from xpublish.plugins import manage
+import xpublish_wms
 
 
 def test_import_plugin():
-    plugins = manage.load_default_plugins()
+    plugins = manage.configure_plugins({"cf_wms": xpublish_wms.CfWmsPlugin})
     assert "cf_wms" in plugins
